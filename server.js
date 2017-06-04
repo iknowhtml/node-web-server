d72ee113486380e0fd9cce3bfe987c5f006e2c99
Aki Gao <xag9bb@virginia.edu>
2017-06-03 20:22:18 -0400

    complete 44. advanced templating

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
@@ -13,14 +23,12 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website.',
-    currentYear: new Date().getFullYear(),
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
   });
 });
 
