3d0df4baa6d1fc9d1b4b1debf6eeb3909c7a38bd
Aki Gao <xag9bb@virginia.edu>
2017-06-03 20:12:07 -0400

    complete 44. rendering templates with data

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
-
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Aki',
-    likes: ['Coding', 'Dancing', 'Working Out'],
+  // res.send({
+  //   name: 'Aki',
+  //   likes: ['Coding', 'Dancing', 'Working Out'],
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website.',
+    currentYear: new Date().getFullYear(),
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear(),
+  });
 });
 
 app.get('/bad', (req, res) => {
